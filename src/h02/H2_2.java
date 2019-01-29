7fa21527890bd911ba93daefbfda73728626912f
RickBodewes <Rick@planet.nl>
2019-01-29 08:40:07 +0100

    eerste commit

---
+++ src/h02/H2_2.java
@@ -0,0 +1,18 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class H2_2 extends Applet{
+
+    public void init(){
+
+    }
+    public void paint(Graphics g){
+        g.setColor(Color.blue);
+        g.drawString("Rick", 20,50);
+        g.setColor(Color.red);
+        g.drawString("Bodewes",20, 60);
+    }
+}
